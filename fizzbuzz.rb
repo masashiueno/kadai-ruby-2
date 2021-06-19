e0ff12ad001f84ce8e3a8f5eea89963ec5193e07
masashi.ueno <masashiueno@outlook.com>
2021-06-19 12:32:38 +0000

    Lesson6-kadai-2nd

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,20 @@
-def sum
-  num_max = 100 #変数
-  (1..num_max).each do |number| #1～num_maxまでeach do
-    if number % 15 == 0 #条件1(またはnumber % 3 == 0 and number % 5 == 0でもよい？？)
-      puts "FizzBuzz" #条件1に合致した場合の行動
-    elsif number % 5 == 0 #条件2
+num_max = 100
+
+def fizzbuzz(num)
+  if num % 15 == 0 #条件1(またはnumber % 3 == 0 and number % 5 == 0でもよい？？)
+    puts "FizzBuzz" #条件1に合致した場合の行動
+  elsif num % 5 == 0 #条件2
       puts "Buzz" #条件2に合致した場合の行動
-    elsif number % 3 == 0 #条件3
+  elsif num % 3 == 0 #条件3
       puts "Fizz" #条件3に合致した場合の行動
-    else puts number #条件1～3に合致しないものはそのナンバーを書く
-    end
+  else puts num #条件1～3に合致しないものはそのナンバーを書く
   end
 end
 
-sum
+
+
+i = 1 #iの初期値
+while i <= num_max  #i値の範囲指定
+  fizzbuzz(i) #fizzbuzz(i)の実行
+  i+= 1 #i値の加算(1→2へ、2→3へ....num_maxへ)
+end
